711d6d59199d8665d072bbbc47c51c4ed974fe82
akphp <akramalsaidi9@gmail.com>
2020-02-17 13:21:37 +0200

    Initial commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Cookie;
+use Illuminate\Support\Facades\DB;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            // 'tracking_id' => 'required'
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        $referred_by = Cookie::get('referral');
+        // $referrer_id = DB::table('users')->where('affiliate_id', $referred_by)->value('id');
+
+        // dd($referred_by);
+        if ($referred_by) {
+            $u = User::where('affiliate_id', $referred_by)->first();
+            User::where('affiliate_id', $referred_by)->update([
+                'points' => $u->points + 10,
+            ]);
+        }
+        $points = 10;
+        if ($referred_by == NULL && $referred_by  == '') {
+            $points = 0;
+        }
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'affiliate_id' => Str::random(10),
+            'referred_by'   => $referred_by,
+            'points' => $points
+        ]);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <h1>You are logged in!</h1>
+                    <br/>
+                    
+                    {{-- <input type="text" readonly="readonly"  --}}
+                        {{-- value="{{url('/').'/?ref='.Auth::user()->affiliate_id}}"> --}}
+                
+
+                @if(Auth::user()->affiliate_id)
+                    <div class="form-group">
+                      <label class="h3" for="exampleInputEmail1"> Url Refarres</label>
+                      <input type="email" class="form-control" readonly="readonly" 
+                      value="{{url('/').'/?ref='.Auth::user()->affiliate_id}}">
+                    </div>
+                
+
+
+                  <div class="card">
+                    <div class="card-body">
+                       <h4 class="text-danger">  Your current total score is :  {{ Auth::user()->points }} </h4>
+                    </div>
+                  </div>
+
+
+                  @endif
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
